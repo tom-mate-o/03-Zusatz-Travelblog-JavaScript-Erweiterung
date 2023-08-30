bd81ee81114954cd52a3f50458ec020488c27134
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2023-08-30 10:31:11 +0200

    darkmode seitenübergreifend

--- script.js
+++ script.js
@@ -17,24 +17,50 @@ document.querySelectorAll(".frage").forEach(function(frageElement) {
     });
 });
 
+// def. funktion zum setzten des farbmodus im local storage
+function setDarkModePreference(darkMode){
+    localStorage.setItem("darkMode", darkMode)
+    loadDarkModePreferences();
+}
 
+// def. funtkion die Farbmodus aus dem local storage lädt
+function loadDarkModePreferences(){
+    const savedDarkMode = localStorage.getItem("darkMode");
+    if (savedDarkMode === "true"){ // wenn true, bekommt der body die css klasse dark mode
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode"); // wenn nicht, wird sie entfernt
+    }
+}
+
+
+// ladet den gespeicherten Farbmodus beim Laden der Seite
+document.addEventListener("DOMContentLoaded", function(){
+    loadDarkModePreferences();
+});
 
 document.getElementById("modeButton").addEventListener("click", function(){
     console.log("CLICK BUTTON");
 
     var buttonElement = document.getElementById("modeButton");
 
-    buttonElement.classList.toggle("darkmode");
-    buttonElement.classList.toggle("lightmode");
+    
 
     var spanElement = buttonElement.querySelector("span");
 
     if (buttonElement.classList.contains("darkmode")) {
-        spanElement.innerHTML = '<i class="fa-solid fa-moon"></i> Dark Mode';
-    } else if (buttonElement.classList.contains("lightmode")) {
         spanElement.innerHTML = '<i class="fa-solid fa-sun"></i> Light Mode';
+        setDarkModePreference(true); // darkMode im Local storage auf true
+    } else if (buttonElement.classList.contains("lightmode")) {
+        spanElement.innerHTML = '<i class="fa-solid fa-moon"></i> Dark Mode';
+        setDarkModePreference(false); //darkMode im Local storage auf false
     }
+
+    buttonElement.classList.toggle("lightmode");
+    buttonElement.classList.toggle("darkmode");
+    
 });
+
 
 
 function modefunktion(){
